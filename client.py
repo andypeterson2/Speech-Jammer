4e579ddf659762327629b899883009f6b908224e
Andrew Peterson <andypeterson526@gmail.com>
2023-08-23 17:00:53 -0700

    Minor refactor, id shortened to 5 characters

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 import socket
 import logging
-import time
 
+# Setup Logging
 logging.basicConfig(level=logging.INFO)
 
 class Client:
@@ -10,21 +10,24 @@ class Client:
         self.port = port
 
     def connect(self):
-        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        try:
-            client_socket.connect((self.host, self.port))
-            unique_id = client_socket.recv(1024).decode().split()[-1]
-            logging.info(f"Received ID {unique_id} from server")
-            while True:
-                query_id = input("Enter a user ID to query: ")
-                client_socket.sendall(query_id.encode())
-                response = client_socket.recv(1024).decode()
-                logging.info(f"Server response: {response}")
-        except:
-            logging.error("Failed to connect or communicate with the server")
-        finally:
-            client_socket.close()
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+            try:
+                client_socket.connect((self.host, self.port))
+                unique_id = client_socket.recv(1024).decode().split()[-1]
+                logging.info(f"Received ID {unique_id} from server")
+                while True:
+                    query_id = input("Enter a user ID to query: ")
+                    client_socket.sendall(query_id.encode())
+                    response = client_socket.recv(1024).decode()
+                    logging.info(f"Server response: {response}")
+            except Exception as e:
+                logging.error(f"Exception while communicating with server: {str(e)}")
+            finally:
+                logging.info("Connection closed")
 
 if __name__ == "__main__":
-    client = Client('localhost', 60532)
-    client.connect()
+  HOST = '127.0.0.1'
+  PORT = 65430
+
+  client = Client(HOST, PORT)
+  client.connect()
--- server.py
+++ server.py
@@ -2,60 +2,75 @@ import socket
 import threading
 import logging
 import hashlib
-import time
 
+# Setup Logging
 logging.basicConfig(level=logging.INFO)
 
-class ClientHandler:
-    def __init__(self, conn, addr, client_ids):
+class UserIDManager:
+    def __init__(self):
+        self.client_ids = {}
+
+    def generate_id(self, addr):
+        addr_str = f"{addr[0]}:{addr[1]}"
+        return hashlib.sha1(addr_str.encode()).hexdigest()[:5]
+
+    def add_id(self, unique_id, addr):
+        self.client_ids[unique_id] = addr
+
+    def get_addr(self, query_id):
+        return self.client_ids.get(query_id, None)
+
+class ClientHandler(threading.Thread):
+    def __init__(self, conn, addr, user_id_manager):
+        super().__init__()
         self.conn = conn
         self.addr = addr
-        self.client_ids = client_ids
+        self.user_id_manager = user_id_manager
 
-    def generate_id(self):
-        ip, port = self.addr
-        ip = socket.gethostbyname(ip) # Resolving hostname to IP
-        unique_str = ip + str(port)
-        return hashlib.sha1(unique_str.encode()).hexdigest()[:10]
+    def run(self):
+        try:
+            self.handle()
+        except Exception as e:
+            logging.error(f"Exception while handling client {self.addr}: {str(e)}")
+        finally:
+            self.conn.close()
+            logging.info(f"Connection closed with {self.addr}")
 
     def handle(self):
         try:
-            unique_id = self.generate_id()
-            self.client_ids[unique_id] = self.addr
+            unique_id = self.user_id_manager.generate_id(self.addr)
+            self.user_id_manager.add_id(unique_id, self.addr)
             logging.info(f"Assigned ID {unique_id} to {self.addr}")
             self.conn.sendall(f"Your ID is {unique_id}".encode())
             while True:
                 query_id = self.conn.recv(1024).decode()
-                if query_id == unique_id:
+                if not query_id:
+                    logging.info(f"Connection terminated by client {self.addr}")
+                    break
+                elif query_id == unique_id:
                     self.conn.sendall(b"That's your own ID.")
-                elif query_id in self.client_ids:
-                    ip, port = self.client_ids[query_id]
-                    self.conn.sendall(f"The IP is {ip} and the port is {port}".encode())
                 else:
-                    self.conn.sendall(b"ID doesn't exist.")
+                    addr = self.user_id_manager.get_addr(query_id)
+                    if addr:
+                        ip, port = addr
+                        self.conn.sendall(f"The IP is {ip} and the port is {port}".encode())
+                    else:
+                        self.conn.sendall(b"ID doesn't exist.")
         except:
             logging.error(f"Failed to handle client {unique_id}")
         finally:
             self.conn.close()
-
-
-class Server:
-    def __init__(self, host, port):
-        self.host = host
-        self.port = port
-        self.client_ids = {}
-        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server_socket.bind((host, port))
-        self.server_socket.listen()
-        logging.info(f"Server is running on {host} and port {port}")
-
-    def start(self):
-        while True:
-            conn, addr = self.server_socket.accept()
-            client_handler = ClientHandler(conn, addr, self.client_ids)
-            thread = threading.Thread(target=client_handler.handle)
-            thread.start()
-
+            
 if __name__ == "__main__":
-    server = Server('localhost', 60532)
-    server.start()
+    HOST = '127.0.0.1'
+    PORT = 65430
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+        server_socket.bind((HOST, PORT))
+        server_socket.listen()
+        logging.info(f"Server listening on {HOST}:{PORT}")
+        user_id_manager = UserIDManager()
+        while True:
+            conn, addr = server_socket.accept()
+            client_handler = ClientHandler(conn, addr, user_id_manager)
+            client_handler.start()
