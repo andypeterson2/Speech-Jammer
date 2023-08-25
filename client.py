54da9eb1c2b64bb559125e9b13f1c2b962107997
Andrew Peterson <andypeterson526@gmail.com>
2023-08-25 08:00:26 -0700

    almost there

--- client.py
+++ client.py
@@ -1,49 +1,127 @@
 import socket
 import logging
+import threading
+import select
 
 # Setup Logging
 logging.basicConfig(level=logging.INFO)
+
+class CommandHandler:
+    def __init__(self, client):
+        self.client = client
+        self.commands = {
+            '1': (self.client.query_id, 'Check if ID exists'),
+            '2': (self.client.ping_server, 'Ping server'),
+            '3': (self.client.connect_to_client_by_id, 'Connect to client'),
+            '4': (self.client.listen_for_connections, 'Listen for connections')
+        }
+
+    def handle_command(self, choice, client_socket):
+        command = self.commands.get(choice, client_socket)
+        if command:
+            command[0](client_socket)
+        else:
+            print("Invalid command. Try again.")
+
 
 class Client:
     def __init__(self, host, port):
         self.host = host
         self.port = port
+        self.user_id = None
+        self.listening_socket = None
+
+    def start_listening(self):
+        self.listening_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.listening_socket.bind((self.host, self.port+2))
+        self.listening_socket.listen(1)
+        logging.info(f"Listening on {self.host}:{self.port}")
+
+    def listen_for_connections(self, _):
+        self.start_listening()
+        while self.listening_socket:
+            readable, _, _ = select.select([self.listening_socket], [], [], 0.1)
+            if readable:
+                conn, addr = self.listening_socket.accept()
+                self.handle_connection(conn)
+
+    def handle_connection(self, conn):
+        with conn:
+            logging.info(f"Connection established with {conn.getpeername()}")
+            conn.sendall(b"Hello from client!")
+            response = conn.recv(1024).decode()
+            logging.info(f"Received: {response}")
+
+    def validate_id(self, client_socket, id):
+        # if id == self.user_id:
+        #     logging.error("Cannot use your own ID.")
+        #     return False
+
+        client_socket.sendall(id.encode())
+        response = client_socket.recv(1024).decode()
+        if response == "ID not found.":
+            logging.error("ID not found.")
+            return False
+
+        return True
 
     def connect(self):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
             try:
                 client_socket.connect((self.host, self.port))
-                unique_id = client_socket.recv(1024).decode().split()[-1]
-                logging.info(f"Received ID {unique_id} from server")
+                self.user_id = client_socket.recv(1024).decode().split()[-1]
+                logging.info(f"Received user ID {self.user_id} from server")
+                command_handler = CommandHandler(self)
                 while True:
-                    print("1) Check if an ID exists\n2) Ping server\n")
+                    for key, value in command_handler.commands.items():
+                        print(f"{key}) {value[1]}")
+
                     choice = input("Select a command: ")
                     client_socket.sendall(choice.encode())
-                    if choice == '1':
-                        self.query_id(client_socket)
-                    elif choice == '2':
-                        self.ping_server(client_socket)
-                    else:
-                        print("Invalid command. Try again.")
+                    command_handler.handle_command(choice, client_socket)
             except Exception as e:
-                logging.error(f"Exception while communicating with server: {str(e)}")
+                logging.error(f"Exception while communicating with server: {str(e)}", exc_info=True)
             finally:
                 logging.info("Connection closed")
 
+    def connect_to_client_by_id(self, client_socket):
+        id = input("Enter the ID of the client to connect to: ")
+        # with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+        if not self.validate_id(client_socket, id):
+            return
+
+        response = client_socket.recv(1024).decode()
+        target_host, target_port = response.split(":")
+        self.handshake_with_client(target_host, int(target_port))
+
+    def handshake_with_client(self, host, port):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+            try:
+                client_socket.connect((host, port))
+                client_socket.sendall(b"Hello!")
+                response = client_socket.recv(1024).decode()
+                logging.info(f"Handshake response: {response}")
+            except Exception as e:
+                logging.error(f"Exception while handshaking with client: {str(e)}", exc_info=True)
+
     def query_id(self, client_socket):
-        query_id = input("Enter a user ID to query: ")
-        client_socket.sendall(query_id.encode())
+        id = input("Enter a user ID to query: ")
+        # with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+        client_socket.sendall(id.encode())
         response = client_socket.recv(1024).decode()
         logging.info(f"Server response: {response}")
 
     def ping_server(self, client_socket):
+        # with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
         response = client_socket.recv(1024).decode()
         logging.info(f"Server response: {response}")
 
 
+# Main block
 if __name__ == "__main__":
-  HOST = '127.0.0.1'
-  PORT = 65430
+    logging.basicConfig(level=logging.INFO)
+    HOST = '127.0.0.1'
+    PORT = 65431
 
-  client = Client(HOST, PORT)
-  client.connect()
+    client = Client(HOST, PORT)
+    client.connect()
--- server.py
+++ server.py
@@ -1,14 +1,15 @@
+# Importing required libraries
 import socket
 import threading
 import logging
 import hashlib
 
-# Setup Logging
-logging.basicConfig(level=logging.INFO)
-
 class UserIDManager:
     def __init__(self):
         self.client_ids = {}
+
+    def id_exists(self, user_id):
+        return user_id in self.client_ids
 
     def generate_id(self, addr):
         addr_str = f"{addr[0]}:{addr[1]}"
@@ -20,61 +21,92 @@ class UserIDManager:
     def get_addr(self, query_id):
         return self.client_ids.get(query_id, None)
 
+
 class ClientHandler(threading.Thread):
     def __init__(self, conn, addr, user_id_manager):
         super().__init__()
         self.conn = conn
         self.addr = addr
         self.user_id_manager = user_id_manager
+        self.user_id = None
 
     def run(self):
         try:
             self.handle()
         except Exception as e:
-            logging.error(f"Exception while handling client {self.addr}: {str(e)}")
+            logging.error(f"Exception while handling client {self.addr}: {str(e)}", exc_info=True)
         finally:
             self.conn.close()
             logging.info(f"Connection closed with {self.addr}")
 
     def handle(self):
-        try:
-            unique_id = self.user_id_manager.generate_id(self.addr)
-            self.user_id_manager.add_id(unique_id, self.addr)
-            logging.info(f"Assigned ID {unique_id} to {self.addr}")
-            self.conn.sendall(f"Your ID is {unique_id}".encode())
-            while True:
-                command = self.conn.recv(1024).decode()
-                if not command:
-                    logging.info(f"Connection terminated by client {self.addr}")
-                    break
-                
-                if command == '1':
-                    self.check_id_existence()
-                elif command == '2':
-                    self.ping()
-        except:
-            logging.error(f"Failed to handle client {unique_id}")
-        finally:
-            self.conn.close()
+        self.user_id = self.user_id_manager.generate_id(self.addr)
+        self.user_id_manager.add_id(self.user_id, self.addr)
+        logging.info(f"Assigned ID {self.user_id} to {self.addr}")
+        self.conn.sendall(f"Your ID is {self.user_id}".encode())
+        while True:
+            command = self.conn.recv(1024).decode()
+            if not command:
+                logging.info(f"Connection terminated by client {self.addr}")
+                break
+            self.process_command(command)
+
+    def process_command(self, command):
+        handlers = {
+            '1': self.check_id_existence,
+            '2': self.ping,
+            '3': self.connect_to_client,
+            '4': self.toggle_client_listen
+        }
+        handler = handlers.get(command)
+        if handler:
+            handler()
+        else:
+            logging.warning(f"Unknown command {command} from client {self.user_id}{self.addr}")
+
+    def toggle_client_listen(self):
+        logging.info(f"Toggle client listen command received. Current server port: {self.addr[1]}")
+
+    def connect_to_client(self):
+        logging.info(f"Connect to client command received from {self.addr}")
+        
+        query_id = self.conn.recv(1024).decode()
+        if query_id == self.user_id:
+            self.conn.sendall("Cannot use your own ID.".encode())
+            logging.warning(f"User {self.user_id} attempted to use their own ID. Command failed.")
             
+            return
+
+        if not self.user_id_manager.id_exists(query_id):
+            self.conn.sendall("ID not found.".encode())
+            logging.warning(f"ID {query_id} not found.")
+            
+            return
+        
+        target_addr = self.user_id_manager.get_addr(query_id)
+        if target_addr is None:
+            logging.error(f"Failed to get address for ID {query_id}")
+            self.conn.sendall("Failed to get address.".encode())
+        else:
+            response = f"{target_addr[0]}:{target_addr[1]}"
+            self.conn.sendall(response.encode())
+            logging.info(f"Sent address {response} to client {self.addr}")
+
     def check_id_existence(self):
         query_id = self.conn.recv(1024).decode()
         logging.info(f"Received ID check request from IP: {self.addr[0]}, Port: {self.addr[1]}")
-                
-        if query_id in self.user_id_manager.client_ids:
-            client_addr = self.user_id_manager.get_addr(query_id)
-            response = f"ID {query_id} exists with IP: {client_addr[0]} and Port: {client_addr[1]}"            
-        else:
-            response = "ID does not exist."
+        response = "ID does not exist." if not self.user_id_manager.id_exists(query_id) else f"ID {query_id} exists with address {self.user_id_manager.client_ids[query_id]}"
         self.conn.sendall(response.encode())
 
     def ping(self):
-        logging.info(f"Ping received from client {self.addr}")        
+        logging.info(f"Ping received from client {self.addr}")
         self.conn.sendall(b"Pong")
-            
+
+
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
     HOST = '127.0.0.1'
-    PORT = 65430
+    PORT = 65431
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
         server_socket.bind((HOST, PORT))
@@ -85,3 +117,4 @@ if __name__ == "__main__":
             conn, addr = server_socket.accept()
             client_handler = ClientHandler(conn, addr, user_id_manager)
             client_handler.start()
+
