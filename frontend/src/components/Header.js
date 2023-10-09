99813bcebf4a86a75be5848eebbe5e4c2cddd715
Aaron Kirk <36803028+aaron-kirk@users.noreply.github.com>
2023-10-08 22:12:52 -0700

    Added status indicator to header component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,3 +1,4 @@
+import StatusWidget from "./widgets/StatusWidget";
 import Logo from "../assets/Logo.png";
 // import Waveforms from "../assets/Waveforms.png";
 
@@ -11,27 +12,11 @@ import "../css/Header.css";
  */
 export default function Header(props) {
 
-    function getStatusText(status) {
-        switch(status) {
-            case "waiting":
-                return "Establishing Connection ..."
-
-            case "good":
-                return "Communcations Secure"
-
-            case "bad":
-                return "Eavesdropper Detected"
-
-            default:
-                return ""
-        }
-    }
-
     return(
         <div className="header">
             <img src={Logo} alt="UCSD Logo" id="logo" />
             <h1>QKD Video Chat</h1>
-            <div class={`status ${(props.status)}`}>{getStatusText(props.status)}</div>
+            <StatusWidget status={props.status} />
         </div>
     );
 }
