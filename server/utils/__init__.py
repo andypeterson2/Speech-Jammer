faf08a4fbc96e972a68e56d1d700bfc7d22a08b2
Spacefroyo <pygao@ucsd.edu>
2024-02-27 17:14:38 -0800

    hot fix
    violating DRY, OCP, and SRP at the same time but i want my code to work

--- server/utils/__init__.py
+++ server/utils/__init__.py
@@ -1,3 +1,18 @@
+from enum import Enum
+from functools import total_ordering
+@total_ordering
+class ClientState(Enum):
+    NEW = 'NEW' # Uninitialized
+    INIT = 'INIT' # Initialized 
+    LIVE = 'LIVE' # Connected to server
+    CONNECTED = 'CONNECTED' # Connected to peer
+
+    def __lt__(self, other):
+        if self.__class__ is other.__class__:
+            arr = list(self.__class__)
+            return arr.index(self) < arr.index(other)
+        return NotImplemented
+
 class ServerError(Exception):
     pass
 class BadGateway(Exception):
