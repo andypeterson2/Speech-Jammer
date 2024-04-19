750c4dc7df1baa5359f5829343de7158f216397a
Aaron <aaron.awesome.kirk@gmail.com>
2024-04-19 12:56:18 -0700

    Adjust file structure
    I prefer that all the rendered stuff be in /renderer. I moved /components there and separated the pages into a /pages directory.

---
+++ frontend-new/src/renderer/pages/Start.tsx
@@ -0,0 +1,40 @@
+import { useNavigate } from 'react-router-dom';
+import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from '../components/Header';
+
+import './css/Start.css';
+
+function Hello() {
+  const navigate = useNavigate();
+
+  const handleStart = () => {
+    const code = Math.random().toString().slice(2, 10); // TODO: Remove this part entirely
+    navigate(`/session/host/${code}`);
+  };
+
+  const handleJoin = () => {
+    navigate('./join');
+  };
+
+  return (
+    <>
+      <Header />
+      <div className="start-content">
+        <div className="codeForm">
+          {/* TODO: Make these buttons be components instead */}
+          <button type="button" onClick={handleStart}>Start Session</button>
+          <button type="button" onClick={handleJoin}>Join Session</button>
+        </div>
+      </div>
+    </>
+  );
+}
+export default function Start() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Hello />} />
+      </Routes>
+    </Router>
+  );
+}
